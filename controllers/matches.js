d6c9a5254460137119e6a64ff0d879d93f7521b9
David Adeogun <adeogundavidt@gmail.com>
2023-12-05 07:07:45 -0500

    Added coach schema to the model/coaches file

--- controllers/matches.js
+++ controllers/matches.js
@@ -1,4 +1,8 @@
 // Import the Mongoose Library
+
+const request = require('supertest');
+const express = require('express');
+const router = express.Router();
 const mongoose = require("mongoose");
 // Import the matches model (create a models/matches.js file)
 const Matches = require("../models/matches");
@@ -30,30 +34,32 @@ const getMatchesByTeamId = async (req, res) => {
   //#swagger.tags=["matches"]
   try {
     const teamId = req.params["Team ID"];
-    const match = await Matches.findById(teamId);
+    const match = await Matches.findById({"Team ID": teamId});
     res.status(200).json(match);
   } catch (error) {
     console.error("Error fetching match:", error);
     res.status(500).json({ error: "Error fetching match" });
   }
 };
+
 const createMatch = async (req, res) => {
   //#swagger.tags=["matches"]
   try {
-    const match = {
+    const matchData = {
       matchId: req.body["Match ID"],
-      date: req.body.Date,
+      date: new Date(req.body.Date), // Converting to Date object
       teamsInvolved: req.body["Teams Involved"],
       score: req.body.Score,
       stadium: req.body.Stadium,
-      goals: req.body.Goals,
+      goals: req.body.Goals.map(goal => ({
+        playerId: goal["Player ID"],
+        time: parseInt(goal.Time) // Ensure time is a number
+      })),
     };
-    const newMatch = await Matches.create(match);
-    res.status(204).json(newMatch);
+    const newMatch = await Matches.create(matchData);
+    res.status(201).json(newMatch); // 201 for successful creation
   } catch (error) {
     console.error("Error creating match:", error);
-
-    // Respond with a 500 Internal Server Error status and a more specific error message
     res.status(500).json({
       error: "Error creating match. Check the server logs for more details.",
     });
@@ -63,21 +69,21 @@ const createMatch = async (req, res) => {
 const updateMatch = async (req, res) => {
   //#swagger.tags=["matches"]
   try {
-    const matchId = req.params["Match ID"];
-    const match = {
-      matchId: matchId,
-      date: req.body.Date,
+    const matchId = req.params["Match ID"]; // Assuming the param is named 'matchId'
+    const matchData = {
+      date: new Date(req.body.Date),
       teamsInvolved: req.body["Teams Involved"],
       score: req.body.Score,
       stadium: req.body.Stadium,
-      goals: req.body.Goals,
+      goals: req.body.Goals.map(goal => ({
+        playerId: goal["Player ID"],
+        time: parseInt(goal.Time)
+      })),
     };
-    const updatedMatch = await Matches.Update(match);
-    res.status(204).json(updatedMatch);
+    const updatedMatch = await Matches.findOneAndUpdate({ matchId }, matchData, { new: true });
+    res.status(200).json(updatedMatch); // 200 for successful update
   } catch (error) {
     console.error("Error updating match:", error);
-
-    // Respond with a 500 Internal Server Error status and a more specific error message
     res.status(500).json({
       error: "Error updating match. Check the server logs for more details.",
     });
@@ -100,6 +106,33 @@ const deleteMatch = async (req, res) => {
   }
 };
 
+
+
+
+
+router.get('/:Match ID', getSingleMatch); // the function you are testing
+
+jest.mock('../models/matches');
+
+describe('Test the getSingleMatch function', () => {
+    test('It should respond with a match for valid ID', async () => {
+        const match = { _id: 'M001', name: 'Match 1' };
+        Matches.findById.mockResolvedValue(match);
+
+        const response = await request(router).get('/M001');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual(match);
+    });
+
+    test('It should respond with 500 for database error', async () => {
+        Matches.findById.mockRejectedValue(new Error('Error fetching match'));
+
+        const response = await request(router).get('/M001');
+        expect(response.statusCode).toBe(500);
+        expect(response.body).toEqual({ error: 'Error fetching match' });
+    });
+});
+
 module.exports = {
   getAllMatches,
   getSingleMatch,
@@ -108,3 +141,6 @@ module.exports = {
   updateMatch,
   deleteMatch
 };
+
+
+
