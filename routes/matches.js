516dead3cd828acf7b8ef1a199d9185736943c6b
Samuele Leoni <leo22009@byui.edu>
2023-12-08 19:03:45 +0100

    Added authentication check to routes

--- routes/matches.js
+++ routes/matches.js
@@ -2,11 +2,13 @@
 const router = require("express").Router();
 const matchesController = require("../controllers/matches");
 
+const { isAuthenticated } = require("../middleware/authenticate.js");
+
 router.get("/", matchesController.getAllMatches);
 router.get("/:Match_ID", matchesController.getMatchById);
 router.get("/:Team_ID", matchesController.getMatchesByTeamId);
-router.post("/", matchesController.createMatch);
-router.put("/:Match_ID", matchesController.updateMatch);
-router.delete("/:Match_ID", matchesController.deleteMatch);
+router.post("/", isAuthenticated, matchesController.createMatch);
+router.put("/:Match_ID", isAuthenticated, matchesController.updateMatch);
+router.delete("/:Match_ID", isAuthenticated, matchesController.deleteMatch);
 
 module.exports = router;
--- routes/teams.js
+++ routes/teams.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const teamsController = require("../controllers/teams.js");
 
+const { isAuthenticated } = require("../middleware/authenticate.js");
+
 router.get("/", teamsController.getAllTeams);
 router.get("/:Team_ID", teamsController.getTeamById);
-router.post("/", teamsController.createTeam);
-router.put("/:Team_ID", teamsController.updateTeam);
-router.delete("/:Team_ID", teamsController.deleteTeam);
+router.post("/", isAuthenticated, teamsController.createTeam);
+router.put("/:Team_ID", isAuthenticated, teamsController.updateTeam);
+router.delete("/:Team_ID", isAuthenticated, teamsController.deleteTeam);
 
 module.exports = router;
